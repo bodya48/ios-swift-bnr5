763670ef8183d5abce4b84bd984ac3d1f33def03
Bogdan Laukhin <bogdanlaukhin@Alexs-Mac-mini.local>
2016-03-09 13:09:59 +0200

    initial commit

---
+++ chapter01/quiz/quiz/ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  quiz
+//
+//  Created by Bogdan Laukhin on 3/9/16.
+//  Copyright © 2016 Bogdan Laukhin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var questionLabel:    UILabel!
+    @IBOutlet var answerLabel:      UILabel!
+    
+    @IBAction func showNextQuestion(sender: AnyObject) {
+        
+    }
+    
+    @IBAction func showAnswer(sender: AnyObject) {
+        
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+
+}
+
